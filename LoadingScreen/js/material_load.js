4c0a018c334ca3fcd21c9eeec58f6206544ff59c
Loqrin <loqrin@gmail.com>
2017-07-11 16:32:32 +0200

    Initial Release

---
+++ LoadingScreen/js/material_load.js
@@ -0,0 +1,105 @@
+$(document).ready(function ()
+{
+	//Loading Config from Json//
+	$.getJSON("config.json", function(config)
+	{
+		var useBackgroundColour = false;
+		var useBackgroundImage = false;
+
+		if(config[0].useBackgroundColour == "true")
+		{
+			$("#background").css("background-color", "rgb(" + config[0].backgroundColour + ")", "!important");
+		}
+
+		if(config[0].useBackgroundImage == "true")
+		{
+			$("#background").css("background-image", "url('" + config[0].backgroundImagePath + "')");
+		}
+
+		if(config[0].useTextColour == "true")
+		{
+			for(var i = 1; i < 6; i++)
+			{
+				$("#useTextColour" + i).css("color", "rgb(" + config[0].textColour + ")");
+			}
+
+			$("#serverTitle").css("color", "rgb(" + config[0].textColour + ")");
+
+			console.log("true");
+		}
+		else if(config[0].useTextColour == "false")
+		{
+			for(var i = 1; i < 6; i++)
+			{
+				$("#useTextColour" + i).css("color", "rgb(255,255,255)");
+			}
+			
+			$("#serverTitle").css("color", "rgb(255,255,255)");
+
+			console.log("false");
+		}
+
+		if(config[0].useBoldText == "true")
+		{
+			for(var i = 1; i < 6; i++)
+			{
+				$("#useTextColour" + i).addClass("bold-text");
+			}
+
+			$("#serverTitle").addClass("bold-text");
+		}
+		else if(config[0].useBoldText == "false")
+		{
+			for(var i = 1; i < 6; i++)
+			{
+				$("#useTextColour" + i).addClass("light-text");
+			}
+
+			$("#serverTitle").addClass("light-text");
+		}
+
+		if(config[0].playAudioInBackground == "true")
+		{
+			var audio = new Audio('' + config[0].audioPath);
+			audio.play();
+  			audio.volume = config[0].audioVolume;
+		}
+
+		$("#serverTitle").text(config[0].serverTitle);
+
+		for(var i = 1; i < 8; i++)
+		{
+			$("#rule" + i).text(config[1]["rule" + i]);
+		}
+
+		for(var i = 1; i < 8; i++)
+		{
+			$("#servers" + i).text(config[2]["servers" + i]);
+		}
+
+		for(var i = 1; i < 8; i++)
+		{
+			$("#content" + i).text(config[3]["content" + i]);
+		}
+	});
+
+	//Animating Server Title//
+	animateServerTitle_Add();
+
+
+	function animateServerTitle_Remove()
+	{
+		$("#serverTitle").removeClass("pulse");
+		$("#serverTitle").removeClass("animated");
+
+		setTimeout(animateServerTitle_Add, 1500);
+	}
+
+	function animateServerTitle_Add()
+	{
+		$("#serverTitle").addClass("pulse");
+		$("#serverTitle").addClass("animated");
+
+		setTimeout(animateServerTitle_Remove, 1500);
+	}
+});
---
+++ __resource.lua
@@ -0,0 +1,40 @@
+description '[LoqScript] material_load - A material designed loading screen for RP servers. Made by Loqrin : github.com/Loqrin'
+
+files {
+    --Loading Index File--
+    'LoadingScreen/index.html',
+    --Loading Image Files--
+    'LoadingScreen/images/backgroundImg1.jpg',
+    'LoadingScreen/images/backgroundImg2.jpg',
+    'LoadingScreen/images/backgroundImg3.jpg',
+    'LoadingScreen/images/backgroundImg4.jpg',
+    'LoadingScreen/images/backgroundImg5.jpg',
+    'LoadingScreen/images/backgroundImg6.jpg',
+    'LoadingScreen/images/backgroundImg7.jpg',
+    'LoadingScreen/images/backgroundImg8.jpg',
+    --Loading Audio Files--
+    'LoadingScreen/audio/gtavthemesong.ogg',
+    --Loading Config File--
+    'LoadingScreen/config.json',
+    --Loading JS Files--
+    'LoadingScreen/js/materialize.min.js',
+    'LoadingScreen/js/material_load.js',
+    'LoadingScreen/js/jquery.min.js',
+    --Loading CSS Files--
+    'LoadingScreen/css/animate.css',
+    'LoadingScreen/css/materialize.min.css',
+    'LoadingScreen/css/styles.css',
+    --Loading Font Files--
+    'LoadingScreen/fonts/roboto/Roboto-Bold.woff',
+    'LoadingScreen/fonts/roboto/Roboto-Bold.woff2',
+    'LoadingScreen/fonts/roboto/Roboto-Light.woff',
+    'LoadingScreen/fonts/roboto/Roboto-Light.woff2',
+    'LoadingScreen/fonts/roboto/Roboto-Medium.woff',
+    'LoadingScreen/fonts/roboto/Roboto-Medium.woff2',
+    'LoadingScreen/fonts/roboto/Roboto-Regular.woff',
+    'LoadingScreen/fonts/roboto/Roboto-Regular.woff2',
+    'LoadingScreen/fonts/roboto/Roboto-Thin.woff',
+    'LoadingScreen/fonts/roboto/Roboto-Thin.woff2'
+}
+
+loadscreen 'LoadingScreen/index.html'
